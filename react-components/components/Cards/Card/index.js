3f3d8e8271b2afd56fb0aa95ae739f92c285d0c3
Karinaguseva <karinguseva@gmail.com>
2023-03-09 15:07:04 +0300

    Updates

--- react-components/components/Cards/Card/index.js
+++ react-components/components/Cards/Card/index.js
@@ -7,7 +7,7 @@ class Card extends Component {
         this.state = { src: '' };
     }
     loadImage(imageName) {
-        import('https://karinaguseva.github.io/React2023Q1/react-components/data/' + imageName).then((image) => {
+        import('./../../../data/' + imageName).then((image) => {
             this.setState({
                 src: image.default,
             });
