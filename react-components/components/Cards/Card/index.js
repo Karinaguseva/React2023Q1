66445b71ab81e923889dda5c7b442873a778ce96
Karinaguseva <karinguseva@gmail.com>
2023-03-09 17:51:43 +0300

    Updates

--- react-components/components/Cards/Card/index.js
+++ react-components/components/Cards/Card/index.js
@@ -13,12 +13,20 @@ class Card extends Component {
             });
         });
     }
+    // getImgUrl(name: string) {
+    //   return new URL(`${name}`, import.meta.url).href;
+    // }
     render() {
-        this.loadImage(this.props.data.image);
+        // const baseUrl = new URL(import.meta.env.BASE_URL, import.meta.url).href;
+        const imgUrl = new URL(this.props.data.image, import.meta.url).href;
+        // const url = new URL(this.props.data.image, imgUrl).href;
+        console.log(import.meta.url);
+        console.log(import.meta.env.BASE_URL);
+        console.log(this.props.data.image);
+        // this.loadImage(this.props.data.image);
         return (React.createElement("div", { className: "card" },
             React.createElement("div", { className: "card__header" },
-                React.createElement("img", { src: this.props.data.image, alt: this.props.data.title, className: "card__img" }),
-                React.createElement("div", null),
+                React.createElement("img", { src: imgUrl, alt: this.props.data.title, className: "card__img" }),
                 React.createElement("div", null,
                     React.createElement("div", { className: "card__title" }, this.props.data.title),
                     React.createElement("div", { className: "card__description" },
