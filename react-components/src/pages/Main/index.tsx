c9675b37a36b39316768e1dc690737ef81802e2d
Karinaguseva <karinguseva@gmail.com>
2023-03-28 18:37:47 +0300

    refactor: change saving to localStorage on Unmount

--- react-components/src/pages/Main/index.tsx
+++ react-components/src/pages/Main/index.tsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Search from 'components/Search';
 import Cards from '../../components/Cards';
 
 const Main = () => {
   const [search, setSearch] = useState(localStorage.getItem('search.value') || '');
+  const searchRef = useRef(search);
 
-  const setSearchToLocal = (event: React.ChangeEvent<HTMLInputElement>) => {
-    localStorage.setItem('search.value', event.currentTarget.value);
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('search.value', searchRef.current || '');
+    };
+  }, [searchRef]);
+
+  const changeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.currentTarget.value);
+    searchRef.current = event.target.value;
   };
 
   return (
     <main className="main">
-      <Search search={search} setSearch={setSearchToLocal}></Search>
+      <Search search={search} setSearch={changeSearch}></Search>
       <Cards search={search} />
     </main>
   );
