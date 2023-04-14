d81358225d414d8ad557645d7db13e3a73053e4e
Karinaguseva <karinguseva@gmail.com>
2023-04-14 19:21:55 +0300

    Updates

--- react-components/app.test.js
+++ react-components/app.test.js
@@ -1,19 +1,21 @@
-"use strict";
-// import React from 'react';
-// import { screen, render, waitFor } from '@testing-library/react';
-// import { App } from './App';
-// import userEvent from '@testing-library/user-event';
-// describe('App', () => {
-//   it('renders Forms component on navigate', async () => {
-//     render(<App />);
-//     const main = screen.getByText(/Main page/i);
-//     expect(main).toBeInTheDocument();
-//     const link = screen.getByText('Forms');
-//     const user = userEvent.setup();
-//     await user.click(link);
-//     waitFor(async () => {
-//       const forms = await screen.findByText(/Forms page/i);
-//       expect(forms).toBeInTheDocument();
-//     });
-//   });
-// });
+import React from 'react';
+import { screen, render, waitFor } from '@testing-library/react';
+import { App } from './App';
+import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+describe('App', () => {
+    it('renders Forms component on navigate', async () => {
+        render(React.createElement(Provider, { store: store },
+            React.createElement(App, null)));
+        const main = screen.getByText(/Main page/i);
+        expect(main).toBeInTheDocument();
+        const link = screen.getByText('Forms');
+        const user = userEvent.setup();
+        await user.click(link);
+        waitFor(async () => {
+            const forms = await screen.findByText(/Forms page/i);
+            expect(forms).toBeInTheDocument();
+        });
+    });
+});
