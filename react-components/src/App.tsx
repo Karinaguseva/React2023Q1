9d9fd0b9862a88f26b1a26c1e517ae4f6453006e
Karinaguseva <karinguseva@gmail.com>
2023-04-30 17:45:47 +0300

    refactor: delete unnecessary comments

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,69 +1,25 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import {
-  // BrowserRouter,
-  // RouterProvider,
-  // createRoutesFromElements,
-  Route,
-  // createHashRouter,
-  Routes,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import About from 'pages/About';
 import Main from 'pages/Main';
 import NotFound404 from 'pages/NotFound404';
-// import Root from './Root';
 import './styles/style.css';
-
 import ROUTES from 'types/routes';
 import Forms from './pages/Forms/index';
-// import Root from './Root';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-// const router = createHashRouter(
-//   createRoutesFromElements(
-//     <Route path={ROUTES.MAIN} element={<Root />}>
-//       <Route index element={<Main />}></Route>
-//       <Route path={ROUTES.ABOUT} element={<About />}></Route>
-//       <Route path={ROUTES.FORMS} element={<Forms />}></Route>
-//       <Route path="*" element={<NotFound404 />}></Route>
-//     </Route>
-//   )
-// );
-
-// export const App = () => <RouterProvider router={router} />;
 
 export const App = (): JSX.Element => {
   return (
     <>
       <Header />
       <Routes>
-        {/* <Route path={ROUTES.MAIN} element={<Root />}> */}
         <Route index element={<Main />}></Route>
         <Route path={ROUTES.ABOUT} element={<About />}></Route>
         <Route path={ROUTES.FORMS} element={<Forms />}></Route>
         <Route path="*" element={<NotFound404 />}></Route>
-        {/* </Route> */}
       </Routes>
       <Footer />
     </>
   );
 };
-
-// const root = document.getElementById('root') as HTMLElement;
-
-// if (root)
-//   ReactDOM.createRoot(root).render(
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   );
-
-// ReactDOM.hydrateRoot(
-//   document.getElementById('root') as HTMLElement,
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>
-// );
